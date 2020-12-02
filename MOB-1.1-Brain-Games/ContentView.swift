6dfc7b08730d2ebb67acaa84009911d88d431e04
Aetrix27 <davedogg323@gmail.com>
2020-12-01 20:26:26 -0800

    Initial Commit

---
+++ MOB-1.1-Brain-Games/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MOB-1.1-Brain-Games
+//
+//  Created by David Guerrero on 12/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MOB-1.1-Brain-Games/MOB_1_1_Brain_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOB_1_1_Brain_GamesApp.swift
+//  MOB-1.1-Brain-Games
+//
+//  Created by David Guerrero on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct MOB_1_1_Brain_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
